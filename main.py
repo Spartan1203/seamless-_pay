19d96aee70625397a8bab4cf279af4b40221d5af
Spartan1203 <54310316+Spartan1203@users.noreply.github.com>
2023-10-11 10:17:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ st.set_page_config(page_title="Visa Visualizations",
                    layout="wide")
 st.title(":blue[Exploratory Data Analysis of Visa Applications from Africa]")
 uploaded_file=st.file_uploader("https://docs.google.com/spreadsheets/d/1AikiRruWamjk8XcAU_5Pteq8QWGxltDRHExSPauP3v4/edit#gid=1694660000",type=['xslx'])
-if uploaded_file is not 
-None:
-  workbook=pd.ExcelFile(uploaded_file)
-  worksheet=workbook.parse(wokrbook.sheet_names['f1_data'])
-  st.table(worksheet)
+workbook=pd.ExcelFile(uploaded_file)
+worksheet=workbook.parse(wokrbook.sheet_names['f1_data'])
+st.table(worksheet)
 #uploadedFile = st.file_uploader(dataset.xlsx, type='xlsx',accept_multiple_files=False,key="fileUploader")
 #url= "https://github.com/Spartan1203/seamless-_pay/blob/main/dataset.xlsx"
 #response=requests.get(url)
