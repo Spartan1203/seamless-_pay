ad02f7ea975503cd618106049ed793be82b5e2b4
Spartan1203 <54310316+Spartan1203@users.noreply.github.com>
2023-10-11 15:41:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ f1_approved=pd.read_excel(
 b1b2=pd.read_excel(
     "data/seamless_pay.xlsx",
     engine="openpyxl",
-    sheet_name="b1b2_data",
+    sheet_name="b1b2",
     header=0,
     usecols="A:K")
 st.dataframe(f1_approved)
